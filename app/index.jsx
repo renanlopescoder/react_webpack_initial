4e464cc02c6ffad8c72db46137dbd0bf851c1c2e
Renan Lopes <renanlopescoder@gmail.com>
2017-07-07 20:41:10 -0300

    feature: added sample with class component
    refactor: changed folder name sample09_component_utils to sample09_component_service

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Family from './family'
-import Member from './member'
+import ClassComponent from './classComponent'
 
 ReactDOM.render(
-  <Family lastName='Lopes'>
-    <Member name='Renan' />
-    <Member name='Marcelo' />
-  </Family>
+  <ClassComponent value="Class Component" />
   , document.getElementById('app')
 )
