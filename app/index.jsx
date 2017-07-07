8a6cd0edcdc87719307e817f173d222b0134b2e7
Renan Lopes <renanlopescoder@gmail.com>
2017-07-07 10:10:09 -0300

    feature: configuration and component samples
    feature: sample of component with direct html
    feature: sample of component using a function
    feature: sample of component using arrow function
    feature: sample of component using props
    feature: sample of two components

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Primeiro, { Segundo } from './component'
+
+ReactDOM.render(
+  <div>
+    <Primeiro value="Show 1"/>
+    <Segundo value="Show 2" />
+  </div>
+  
+  , document.getElementById('app')
+)
---
+++ sample_components/component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default props => (
+  <h1>Primeiro {props.value}</h1>
+)
+
+export const Segundo = props => (
+  <h1>Segundo {props.value}</h1>
+)
