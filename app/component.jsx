4f5fe19cfeeb0c9cf37d573535d681187f14c336
Renan Lopes <renanlopescoder@gmail.com>
2017-07-07 10:47:18 -0300

    feature: sample of component dependency

--- app/component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default props => (
-  <h1>Primeiro {props.value}</h1>
-)
-
-export const Segundo = props => (
-  <h1>Segundo {props.value}</h1>
-)
---
+++ sample_component_dependencies/family.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Member from './member'
+
+export default props => (
+  <div>
+    <Member name='Renan' lastName='Lopes' />
+    <Member name='Marcelo' lastName='Rodrigues' />
+    <Member name='Monique' lastName='Lopes' />
+  </div>
+)
